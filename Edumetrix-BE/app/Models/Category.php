64b80adeb27a00062860ad97fa5d3d5602810779
developer-Jimmy <g12347825@gmail.com>
2025-02-14 11:58:14 +0800

    categorycontroller

--- Edumetrix-BE/app/Models/Category.php
+++ Edumetrix-BE/app/Models/Category.php
@@ -22,20 +22,3 @@ class Category extends Model
     }
 }
 
-// app/Models/Teacher.php
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Teacher extends Model
-{
-    use HasFactory;
-
-    protected $fillable = ['name'];
-
-    public function courses()
-    {
-        return $this->hasMany(Course::class);
-    }
-}
